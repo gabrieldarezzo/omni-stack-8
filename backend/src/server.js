897180d2eaa28d89bdb20344e14f31d43cfaf016
Gabriel <gabrieldarezzo@yahoo.com.br>
2019-08-07 16:04:11 -0300

    Add suport of Cors in back

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 const server = express();
 
@@ -7,6 +8,8 @@ const server = express();
 mongoose.connect('mongodb+srv://omni:omni5094@omnistack-y62p6.mongodb.net/omnistack?retryWrites=true&w=majority', {
     useNewUrlParser : true
 });
+
+server.use(cors());
 
 server.use(express.json());
 server.use(routes);
